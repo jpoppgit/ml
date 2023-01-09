110dbbdaa63dd166e5b4c8db665b1eac1c281b05
Jens Popp <jenspopp@MacBook-Pro-von-Jens.local>
2023-01-09 10:13:59 +0100

    Improves logging

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 """
-    Practical example of the k-means algorithm.
+    Practical example of a k-means algorithm.
 
     Inspired by
     https://towardsdatascience.com/understanding-k-means-clustering-in-machine-learning-6a6e67336aa1
@@ -21,7 +21,7 @@ logging.basicConfig(level=logging.INFO)
 num_points = 100
 n_clusters = 2
 logging.info('Number of data points: '+str(n_clusters)+' * '+str(num_points))
-X= -2 * np.random.rand(num_points,2)
+X = -2 * np.random.rand(num_points,2)
 X1 = 1 + 2 * np.random.rand(int(num_points/2),2)
 X[50:100, :] = X1
 
@@ -33,10 +33,11 @@ a_cluster_centers = Kmean.cluster_centers_
 a_labels          = Kmean.labels_
 logging.info('Kmeans parameters: '+str(kmean_params))
 logging.info('Kmeans cluster centers: '+str(a_cluster_centers))
-logging.info('Kmeans cluster labels : '+str(a_labels))
+logging.info('Kmeans cluster labels : #'+str(len(a_labels))+' '+str(a_labels))
 
 # plot it
-print(X.shape)
+#print(X.shape)
+#print(X)
 plt.scatter(X[ : , 0], X[ : , 1], s = 50, c = 'b')
 plt.scatter(a_cluster_centers[0, 0], a_cluster_centers[0, 1], s=200, c='g', marker='s')
 plt.scatter(a_cluster_centers[1, 0], a_cluster_centers[1, 1], s=200, c='r', marker='s')
